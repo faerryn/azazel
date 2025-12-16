86871e7b1b64f336623a9b743dc1559487aca524
Faerryn <alexandre.liao@gmail.com>
2025-12-16 12:24:32 -0500

    Re-export the managers directly from lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
-pub mod component_manager;
-pub mod entity_manager;
+mod component_manager;
+mod entity_manager;
 mod opaque_value_hash_map;
+pub use component_manager::ComponentManager;
+pub use entity_manager::EntityManager;
